c8b4dc353f155eff446bf50560d9a43ab91b1e22
Nicole Cordes <cordes@cps-it.de>
2013-11-22 01:20:28 +0100

    [RELEASE] Release of vcc 1.0.2
    Change-Id: Id07a0ab4e75c3279eaae9c9b50222cc3e0f8520f
    Reviewed-on: https://review.typo3.org/25629
    Reviewed-by: Nicole Cordes
    Tested-by: Nicole Cordes

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "vcc".
  *
- * Auto generated 18-11-2013 16:23
+ * Auto generated 22-11-2013 01:19
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'dependencies' => 'cms',
 	'module' => '',
 	'state' => 'stable',
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'clearCacheOnLoad' => 0,
 	'createDirs' => '',
 	'internal' => '',
@@ -40,7 +40,21 @@ $EM_CONF[$_EXTKEY] = array(
 	'priority' => '',
 	'shy' => '',
 	'uploadfolder' => 0,
-	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"5271";s:16:"ext_autoload.php";s:4:"1c64";s:21:"ext_conf_template.txt";s:4:"8b8d";s:12:"ext_icon.gif";s:4:"7736";s:17:"ext_localconf.php";s:4:"9f06";s:14:"ext_tables.php";s:4:"816b";s:14:"ext_tables.sql";s:4:"d9bc";s:37:"Classes/Hook/DocHeaderButtonsHook.php";s:4:"9e76";s:55:"Classes/Interface/CommunicationServiceHookInterface.php";s:4:"1383";s:40:"Classes/Service/CommunicationService.php";s:4:"1d32";s:43:"Classes/Service/ExtensionSettingService.php";s:4:"3306";s:34:"Classes/Service/LoggingService.php";s:4:"b6f3";s:35:"Classes/Service/TsConfigService.php";s:4:"65b9";s:25:"Documentation/default.vcl";s:4:"2042";s:26:"Documentation/Includes.txt";s:4:"cd93";s:23:"Documentation/Index.rst";s:4:"91e2";s:26:"Documentation/Settings.yml";s:4:"3727";s:43:"Documentation/AdministratorManual/Index.rst";s:4:"9a63";s:46:"Documentation/Images/Introduction/Overview.png";s:4:"2e60";s:36:"Documentation/Introduction/Index.rst";s:4:"8383";s:31:"Documentation/Support/Index.rst";s:4:"384b";s:38:"Resources/Public/Icons/CachePlugin.png";s:4:"4312";}',
+	'_md5_values_when_last_written' =>
+		'a:32:{s:9:"ChangeLog";s:4:"e3be";s:16:"ext_autoload.php";s:4:"1c64";s:21:"ext_conf_template.txt";s:4:"8b8d";s:12:"ext_icon.gif";s:' .
+		'4:"7736";s:17:"ext_localconf.php";s:4:"9f06";s:14:"ext_tables.php";s:4:"816b";s:14:"ext_tables.sql";s:4:"d9bc";s:37:"Classes/Hook/' .
+		'DocHeaderButtonsHook.php";s:4:"ceec";s:55:"Classes/Interface/CommunicationServiceHookInterface.php";s:4:"1383";s:40:"Classes/Servi' .
+		'ce/CommunicationService.php";s:4:"6486";s:43:"Classes/Service/ExtensionSettingService.php";s:4:"c1c7";s:34:"Classes/Service/Loggin' .
+		'gService.php";s:4:"0010";s:35:"Classes/Service/TsConfigService.php";s:4:"a923";s:25:"Documentation/default.vcl";s:4:"2042";s:26:"D' .
+		'ocumentation/Includes.txt";s:4:"cd93";s:23:"Documentation/Index.rst";s:4:"68e5";s:26:"Documentation/Settings.yml";s:4:"3727";s:38:' .
+		'"Documentation/Administration/Index.rst";s:4:"ae09";s:61:"Documentation/Administration/ExtensionConfiguration/Index.rst";s:4:"56b9' .
+		'";s:51:"Documentation/Administration/Installation/Index.rst";s:4:"ecdc";s:45:"Documentation/Administration/PageTs/Index.rst";s:4:"' .
+		'0f1e";s:59:"Documentation/Administration/VarnishConfiguration/Index.rst";s:4:"8b74";s:64:"Documentation/Images/Introduction/Screen' .
+		'shots/IconInEditView.png";s:4:"71dc";s:63:"Documentation/Images/Introduction/Screenshots/IconInWebView.png";s:4:"9bbd";s:58:"Docum' .
+		'entation/Images/Introduction/Screenshots/Messages.png";s:4:"2e60";s:36:"Documentation/Introduction/Index.rst";s:4:"3384";s:48:"Doc' .
+		'umentation/Introduction/Screenshots/Index.rst";s:4:"3140";s:44:"Documentation/Introduction/Support/Index.rst";s:4:"bf43";s:49:"Doc' .
+		'umentation/Introduction/WhatDoesItDo/Index.rst";s:4:"6f61";s:36:"Documentation/Requirements/Index.rst";s:4:"11ce";s:38:"Resources/' .
+		'Public/Icons/CachePlugin.png";s:4:"4312";s:14:"doc/manual.sxw";s:4:"b4b2";}',
 	'suggests' => array(
 	),
 	'conflicts' => '',
