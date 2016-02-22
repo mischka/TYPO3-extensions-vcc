41c405183609b51836a1bb5e81118514468d1ee7
Kay Petzold <kay.petzold@digitalwert.de>
2016-02-22 15:07:21 +0100

    Add first Step for BAN over PID

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,6 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+
+// HTTP Header extension
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['isOutputting']['vcc'] = 'CPSIT\\Vcc\\Hook\\HttpHook->main';
